0ec2791526550532a4c6bd1a303a3abfd941309b
Olexandr <BerezovyiAleksandr@gmail.com>
2015-11-05 11:01:24 +0200

    missed changes

--- src/main/webapp/build.config.js
+++ src/main/webapp/build.config.js
@@ -68,7 +68,7 @@ module.exports = {
             'vendor/summernote/dist/summernote.min.js',
             'vendor/angular/angular.js',
             'vendor/angular-bootstrap/ui-bootstrap-tpls.min.js',
-            'vendor/placeholders/angular-placeholders-0.0.1-SNAPSHOT.min.js',
+            'vendor/placeholders/dist/placeholders.min.js',
             'vendor/angular-ui-router/release/angular-ui-router.min.js',
             'vendor/angular-ui-utils/modules/route/route.min.js',
             'vendor/angular-resource/angular-resource.min.js',
--- src/main/webapp/build/src/app/about/about.js
+++ src/main/webapp/build/src/app/about/about.js
@@ -1,6 +1,6 @@
 angular.module( 'ngBoilerplate.about', [
   'ui.router',
-  'placeholders',
+  //'placeholders',
   'ui.bootstrap'
 ])
 
